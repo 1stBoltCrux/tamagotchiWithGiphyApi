d52688558c7d755b8dd38c724dccecd64c574d07
Jared Lutz and Ted Leary <jared@>
2018-04-23 16:41:08 -0700

    add lessons on api for weather app

---
+++ src/api.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  $('#weatherLocation').click(function() {
+    let city = $('#location').val();
+    $('#location').val("");
+    $.ajax({
+      url: `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=7b3432b7aadd238a1cdc78d2ab30b22d`,
+      type: 'GET',
+      data: {
+        format: 'json'
+      },
+      success: function(response) {
+        $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
+        $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp}.`);
+      },
+      error: function() {
+        $('#errors').text("There was an error processing your request. Please try again.")
+      }
+    });
+  });
+});
